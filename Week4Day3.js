b500c3fb3f988cc5a79434b4cf49ee98f7b7ba4d
Owusuduah Achamfour <owusuachamfour@gmail.com>
2023-07-28 17:53:20 -0400

    Where is Waldo answered

--- Week4Day3.js
+++ Week4Day3.js
@@ -143,6 +143,51 @@ console.log(spliceRightHalfOfArray.indexOf('Fast and Furious'));
 ////////////////////////////////
 
 
+const whereIsWaldo = 
+[["Timmy", "Frank"], "Eggbert",
+["Lucinda", "Jacc", "Neff", "Snoop"],
+["Petunia", ["Baked Goods", "Waldo"]]];
+
+//remove Eggbert
+whereIsWaldo.splice(whereIsWaldo.indexOf('Eggbert'), 1);
+console.log(whereIsWaldo);
+
+//change Neff to No One
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo);
+//access and console.log Waldo
+console.log(whereIsWaldo[2][1][1]);
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+//1.
+for(i=1; i<=20; i++){
+    console.log(i + " "+ "Love me, pet me! HSSSSSS!");
+}
+console.log(); //blank line
+//2.
+//let meow;
+kittyTalk = [
+    '...human...why you taking pictures of me?...',
+    '...the catnip made me do it...',
+    '...why does the red dot always get away...'
+]
+for(i=1; i<=20; i++){
+    if(i%2==0){
+        //a random number that is greater than or equal to 0, but less than or equal to 1
+        //that number is multiplied by 2, so two is the maximum the random number can be
+        meow = Math.floor(Math.random() * 2)
+        console.log(i + " "+ "Love me, pet me! HSSSSSS! " + kittyTalk[meow]);
+    }else{
+    console.log(i + " "+ "Love me, pet me! HSSSSSS!");    
+    }
+}
+
+
+
+
+
 ////////////////////////////////
 //  Excited Kitten
 ////////////////////////////////
